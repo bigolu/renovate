ba3feccc86872107799f9475f5291dec6cc13832
Biggie <code@bigo.lu>
2026-05-03 09:10:17 -0400

    wip

--- renovate.bash
+++ renovate.bash
@@ -13,14 +13,17 @@ set -o pipefail
 shopt -s nullglob
 shopt -s inherit_errexit
 
+args=()
+
 export RENOVATE_CONFIG_FILE="${RENOVATE_CONFIG_FILE:-$PWD/global.json}"
 
 # If a CI run fails, we'll have all the debug information without
 # having to rerun it.
-export LOG_LEVEL='debug'
+export LOG_LEVEL="${LOG_LEVEL:-debug}"
 
 if [[ ''${CI:-} != 'true' ]]; then
 	export RENOVATE_DRY_RUN='full'
+	args+=(--platform=local)
 fi
 
-renovate
+renovate "${args[@]}" "$@"
